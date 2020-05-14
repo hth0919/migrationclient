3b77247acbd72bad0b1a77f0d2a2ee89cd4b8119
unknown <hth0919@gmail.com>
2020-05-14 15:56:35 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/hth0919/migrationclient
+
+go 1.13
+
+require (
+	github.com/hth0919/migrationcontroller v0.0.3
+	k8s.io/apimachinery v0.18.2
+	k8s.io/client-go v12.0.0+incompatible
+)
---
+++ projects.go
@@ -0,0 +1,72 @@
+package v1alpha1
+
+import (
+	"github.com/hth0919/migrationcontroller/pkg/apis/keti/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+)
+
+type ProjectInterface interface {
+	List(opts metav1.ListOptions) (*v1alpha1.MigrationList, error)
+	Get(name string, options metav1.GetOptions) (*v1alpha1.Migration, error)
+	Create(migration *v1alpha1.Migration) (*v1alpha1.Migration, error)
+	Watch(opts metav1.ListOptions) (watch.Interface, error)
+	// ...
+}
+
+type projectClient struct {
+	restClient rest.Interface
+	ns         string
+}
+
+func (c *projectClient) List(opts metav1.ListOptions) (*v1alpha1.MigrationList, error) {
+	result := v1alpha1.MigrationList{}
+	err := c.restClient.
+		Get().
+		Namespace(c.ns).
+		Resource("projects").
+		VersionedParams(&opts, scheme.ParameterCodec).
+		Do().
+		Into(&result)
+
+	return &result, err
+}
+
+func (c *projectClient) Get(name string, opts metav1.GetOptions) (*v1alpha1.Migration, error) {
+	result := v1alpha1.Migration{}
+	err := c.restClient.
+		Get().
+		Namespace(c.ns).
+		Resource("projects").
+		Name(name).
+		VersionedParams(&opts, scheme.ParameterCodec).
+		Do().
+		Into(&result)
+
+	return &result, err
+}
+
+func (c *projectClient) Create(project *v1alpha1.Migration) (*v1alpha1.Migration, error) {
+	result := v1alpha1.Migration{}
+	err := c.restClient.
+		Post().
+		Namespace(c.ns).
+		Resource("projects").
+		Body(project).
+		Do().
+		Into(&result)
+
+	return &result, err
+}
+
+func (c *projectClient) Watch(opts metav1.ListOptions) (watch.Interface, error) {
+	opts.Watch = true
+	return c.restClient.
+		Get().
+		Namespace(c.ns).
+		Resource("projects").
+		VersionedParams(&opts, scheme.ParameterCodec).
+		Watch()
+}
