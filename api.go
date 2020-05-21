50badcd0c3e9fe7a6b78a3b5f11ae1a369542325
unknown <hth0919@gmail.com>
2020-05-21 15:02:31 +0900

    add

--- api.go
+++ api.go
@@ -18,7 +18,7 @@ type ExampleV1Alpha1Client struct {
 
 func NewForConfig(c *rest.Config) (*ExampleV1Alpha1Client, error) {
     config := *c
-    config.ContentConfig.GroupVersion = &schema.GroupVersion{Group: "keti.migration", Version: "v1alpha1"}
+    config.ContentConfig.GroupVersion = &schema.GroupVersion{Group: "keti.migration", Version: "v1"}
     config.APIPath = "/apis"
     config.NegotiatedSerializer = scheme.Codecs.WithoutConversion()
     config.UserAgent = rest.DefaultKubernetesUserAgent()
